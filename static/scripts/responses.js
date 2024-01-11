885d7a71d53bf5f675e18d1189fc213000ca6156
Vinisha verma <vinisha657@gmail.com>
2024-01-12 00:09:37 +0530

    feature signup working

---
+++ static/scripts/responses.js
@@ -0,0 +1,93 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+    if (input=="what is your name")
+    {
+        return "I am a Chatbot";
+    }
+    else if(input=="what's your name")
+    {
+        return "I am a Chatbot";
+    }
+    if (input=="how are you")
+    {
+        return "I am a good! and what about you";
+    }
+    else if (input=="How are you")
+    {
+        return "I am a good! and what about you";
+    }
+    else if (input=="How are you?")
+    {
+        return "I am a good! and what about you";
+    }
+    else if (input=="how are you?")
+    {
+        return "I am a good! and what about you";
+    }
+    else if (input=="How are you ?")
+    {
+        return "I am a good! and what about you";
+    }
+    else if(input=="I am also good")
+    {
+        return "Nice to hear that! Have a great day!! ";
+    }
+    else if(input=="i am also good")
+    {
+        return "Nice to hear that! Have a great day!! ";
+    }
+    else if(input=="i am fine")
+    {
+        return "Nice to hear that! Have a great day!! ";
+    }
+    else if(input=="thank you")
+    {
+        return "Welcome";
+    }
+    else if(input=="Thank you")
+    {
+        return "Welcome";
+    }
+    else if(input=="Thank You")
+    {
+        return "Welcome";
+    }
+    else if(input=="Thanks")
+    {
+        return "Welcome";
+    }
+    else if(input=="thanks")
+    {
+        return "Welcome";
+    }
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+    if( input == "what do you know about banasthali vidyapith"){
+        return "I am getting trained on banasthali data, i will be happy to help you with any queries";
+    }
+    if( input == "banasthali vidyapith"){
+        return "I am getting trained on banasthali data, i will be happy to help you with any queries";
+    }
+    if( input == "Banasthali Vidyapith"){
+        return "I am getting trained on banasthali Vidyapith's data, i will be happy to help you with any queries";
+    }
+    if( input == "Tell me something about Banasthali Vidyapith"){
+        return "Banasthali is one of the five higher education institute in India meant exclusively for Women. Banasthali was founded by Smt. Ratan Shastri and Pandit Hiralal Shastri on October 6, 1935. Banasthali Vidyapith is a fully residential women's higher education institute which offers an integrated system extending from the primary to the Ph.D. level.";
+    }
+    
+
+}
